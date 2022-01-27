abaf3c40e1da2b8c3c417bcfe14286355800f532
Sumit <sumit.sth54@gmail.com>
2022-01-27 16:51:14 +0545

    Added dummy response

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+var cors = require('cors');
+const dummyResponse = require('./test.json');
+
+
+const app = express();
+
+app.use(cors());
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    //res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    res.json(dummyResponse);
+  });
+});
+
+app.listen(5000, () => console.log('Server Started...'));

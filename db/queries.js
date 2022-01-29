88d43e7719e1dfcbb6c63ddda0ea9f331ca6df10
Sumit <sumit.sth54@gmail.com>
2022-01-29 14:45:29 +0545

    Added postGres database and endpoints to interact with the database

---
+++ db/queries.js
@@ -0,0 +1,80 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: 'result',
+  host: 'localhost',
+  database: 'results',
+  password: 'result123',
+  port: 5432,
+})
+
+const insertData = (req, res) => {
+    let message = '';
+    const { studentInfo, tableData, summary } = req.body;
+    const { CRN, name,level,campus,programme, yearpart, examRollNo} = studentInfo;
+    const { date, grandTotal, result } = summary;
+    const subjectData = { "tableData":tableData };
+
+    // console.log(CRN, name , level, campus, programme, yearpart, examRoll, subjectData,date, grandTotal, result );
+    // res.send(req.body);
+    ;(async () => {
+        const client = await pool.connect()
+        try {
+          const response = await client.query('SELECT * FROM student WHERE crn = $1', [CRN])
+          if(!response.rows[0]){
+            client.query('INSERT INTO student (crn, name,level,campus,programme) VALUES ($1, $2, $3, $4, $5)',
+            [CRN, name,level,campus,programme],
+            (error,results) => {
+                if(error)
+                    throw error;
+                message = `Student added with CRN: ${CRN}`
+                //res.status(201).send(`Student added with CRN: ${crn}`);
+        })
+        }
+        client.query('INSERT INTO marksheet (yearpart, examRoll, crn, subjectData, date, grandTotal, result) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        [yearpart, examRollNo, CRN, subjectData, date, grandTotal, result],
+        (error,results) => {
+            if(error)
+                throw error;
+            res.status(201).send(`${message}\nMarksheet added of student with CRN: ${CRN}`);
+        }
+    )
+
+        } finally {
+          // Make sure to release the client before any error handling,
+          // just in case the error handling itself throws an error.
+          client.release()
+        }
+      })().catch(err => console.log(err.stack));
+
+    // pool.query('INSERT INTO student (crn, name,level,campus,programme) VALUES ($1, $2, $3, $4, $5)',
+    //     [CRN, name,level,campus,programme],
+    //     (error,results) => {
+    //         if(error)
+    //             throw error;
+    //         response = `Student added with CRN: ${CRN}`
+    //         //res.status(201).send(`Student added with CRN: ${crn}`);
+    // })
+    // pool.query('INSERT INTO marksheet (yearpart, examRoll, crn, subjectData, date, grandTotal, result) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+    //     [yearpart, examRollNo, CRN, subjectData, date, grandTotal, result],
+    //     (error,results) => {
+    //         if(error)
+    //             throw error;
+    //         res.status(201).send(`${response}\nMarksheet added with CRN: ${CRN}`);
+    //     }
+    // )
+}
+
+const showData = (req, res) => {
+    pool.query('SELECT * FROM student S INNER JOIN marksheet M ON S.crn=M.crn',  (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    insertData,
+    showData
+}
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 var cors = require('cors');
 const dummyResponse = require('./test.json');
-
+const db = require('./db/queries');
 
 const app = express();
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.use(cors());
 app.use(fileUpload());
@@ -27,5 +35,10 @@ app.post('/upload', (req, res) => {
     res.json(dummyResponse);
   });
 });
+
+//Confirm Endpoint
+app.post('/confirm', db.insertData);
+
+app.get('/allData', db.showData);
 
 app.listen(5000, () => console.log('Server Started...'));
